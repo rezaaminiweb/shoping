433efe36f00cdf6ef23fe387c3c8267608ed0474
rezaaminiweb <rezaaminiweb@gmail.com>
2024-03-27 20:10:27 +0330

    second

---
+++ src/app/checkout/page.jsx
@@ -0,0 +1,19 @@
+
+
+const Checkout = ()=>{
+    return(
+        <>
+        <div className="w-full">
+            <div className="container mx-auto">
+                <div className="w-full ">
+
+                </div>
+
+            </div>
+
+        </div>
+        
+        </>
+    )
+}
+export default Checkout
