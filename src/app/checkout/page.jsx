436dda15063de947c84aa3aed25b9dfdd7bf8311
rezaaminiweb <rezaaminiweb@gmail.com>
2024-03-28 05:40:01 +0330

    start form

--- src/app/checkout/page.jsx
+++ src/app/checkout/page.jsx
@@ -4,6 +4,7 @@ import { useContext, useState } from "react"
 import { ShopC } from "../components/context/Shopcontext"
 import DetailSlide from "../components/componentsUI/Detail"
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import StarIcon from '@mui/icons-material/Star';
 
 const Checkout = () => {
     const { cartItems } = useContext(ShopC)
@@ -32,22 +33,85 @@ const Checkout = () => {
                                 </div>
 
                             </div>
-                            <div className="flex flex-col">
-                                <label htmlFor="country" className=" py-4  w-fit">کشور</label>
-                                <div className="relative w-[70%]">
-                                    <select name="country" id="country" className=" w-full outline-none border rounded-sm border-[#707070ed] p-2 appearance-none" onChange={handleCountryChange} value={selectedCountry}>
-                                        <option value="">لطفاً کشور خود را انتخاب کنید</option>
-                                        {countries.map((country, index) => (
-                                            <option key={index} value={country}>
-                                                {country}
-                                            </option>
-                                        ))}
-                                    </select>
-                                    <span className="absolute left-0 top-0 bottom-0 flex items-center pr-2">
-                                        <ExpandMoreIcon />
-                                    </span>
+                            <form action="">
+                                <div className="flex flex-col">
+
+                                    <label htmlFor="country" className=" py-3  w-fit relative">کشور
+                                        <StarIcon className="text-[7px] text-red-500 absolute top-4" />
+                                    </label>
+                                    <div className="relative w-full md:w-[70%]">
+                                        <select name="country" id="country" className=" w-full outline-none border rounded-sm border-[#707070ed] p-2 appearance-none" onChange={handleCountryChange} value={selectedCountry}>
+                                            <option value="">لطفاً کشور خود را انتخاب کنید</option>
+                                            {countries.map((country, index) => (
+                                                <option key={index} value={country}>
+                                                    {country}
+                                                </option>
+                                            ))}
+                                        </select>
+                                        <span className="absolute left-0 top-0 bottom-0 flex items-center pr-2">
+                                            <ExpandMoreIcon />
+                                        </span>
+                                    </div>
                                 </div>
-                            </div>
+                                <div className="flex flex-wrap pt-8 py-4">
+                                    <div className="flex flex-col w-full md:w-[50%]">
+
+                                        <label htmlFor="name" className="relative">
+                                            نام
+                                            <StarIcon className="text-[7px] text-red-500 absolute top-0" />
+
+                                        </label>
+                                        <input type="text" placeholder="رضا" className="px-2 py-1 w-full md:w-[70%] outline-none border rounded-sm border-[#707070ed]" name="" id="name" />
+                                    </div>
+                                    <div className="flex flex-col w-full md:w-[50%] py-4 md:py-0">
+                                        <label htmlFor="family" className="relative">
+                                            نام خانوادگی
+                                            <StarIcon className="text-[7px] text-red-500 absolute top-0" />
+                                        </label>
+                                        <input type="text" name="" placeholder="امینی" id="family" className="px-2 py-1 outline-none border rounded-sm border-[#707070ed] w-full md:w-[70%] " />
+                                    </div>
+
+                                </div>
+                                <div className="py-4 flex flex-col">
+                                    <label htmlFor="companyname" className="relative w-fit py-1">
+                                        نام شرکت
+                                        <StarIcon className="text-[7px] text-red-500 absolute top-0" />
+                                    </label>
+                                    <input type="text" name="" placeholder="فراگستر" id="companyname" className="px-2 py-1 outline-none border rounded-sm border-[#707070ed] w-full  md:w-[70%] " />
+
+                                </div>
+                                <div className="py-4 flex flex-col">
+                                    <label htmlFor="companyname" className="relative w-fit py-1">
+                                        آدرس
+                                        <StarIcon className="text-[7px] text-red-500 absolute top-0" />
+                                    </label>
+                                    <input type="text" name="" placeholder="تهران منطقه 19 " id="companyname" className="px-2 py-1 outline-none border rounded-sm border-[#707070ed] w-full md:w-[70%] " />
+
+                                </div>
+                                <div className="py-4 flex flex-col">
+                                    <label htmlFor="companyname" className="relative w-fit py-1">
+                                        ایمیل
+                                        <StarIcon className="text-[7px] text-red-500 absolute top-0" />
+                                    </label>
+                                    <input type="email" name="" placeholder=" rezaaminiweb@gmail.com" id="companyname" className="px-2 py-1 outline-none border rounded-sm border-[#707070ed] w-full md:w-[70%] " />
+
+                                </div>
+                                <div className="py-4 flex flex-col">
+                                    <label htmlFor="companyname" className="relative w-fit py-1">
+                                        تلفن
+                                        <StarIcon className="text-[7px] text-red-500 absolute top-0" />
+                                    </label>
+                                    <div className="flex  ">
+                                        
+                                        <input type="tel" name="phone" placeholder="+989126059969" id="companyname" className="px-2 py-1 outline-none border rounded-sm border-[#707070ed] w-full md:w-[50%] text-end" />
+                                        <select name="" id="" className="w-[20%] appearance-none">
+                                            <option value="+98" className="flex">+98 IRAN</option>
+                                            <option value="+41">+41 UNITED KINGDOM</option>
+                                        </select>
+                                    </div>
+
+                                </div>
+                            </form>
                         </div>
                         <div className="w-[100%] md:w-[50%] p-3 md:px-5 md:py-6">
 
